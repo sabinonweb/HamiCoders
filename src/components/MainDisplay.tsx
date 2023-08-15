a9cb3f77dd67349c62103cda267fcf6ed8828a11
Sabin <sabinonweb7@gmail.com>
2023-08-16 01:37:49 +0545

    UI Made

--- src/components/MainDisplay.tsx
+++ src/components/MainDisplay.tsx
@@ -5,12 +5,26 @@ function MainDisplay() {
     <section className="MainContainer">
       <div className="container mx-auto px-4 sm:px-6 md:px-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
         </div>
       </div>
     </section>
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,226 @@
+import React from "react";
+import {
+  FireOutlined,
+  ArrowUpOutlined,
+  UngroupOutlined,
+  LinkOutlined,
+  SearchOutlined,
+  CodeOutlined,
+  PauseCircleOutlined,
+  HomeOutlined,
+  PlusCircleOutlined,
+  SoundOutlined,
+  FileTextOutlined,
+  MinusSquareOutlined,
+  AlertOutlined,
+  ShareAltOutlined,
+  CreditCardOutlined,
+  BookOutlined,
+  EyeOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+// import { useState } from "react";
+const Navigation = () => {
+  const feedData = [
+    {
+      name: "My feed",
+      icon: <HomeOutlined />,
+    },
+    {
+      name: "New Squad",
+      icon: <PlusCircleOutlined />,
+    },
+  ];
+
+  const discoverData = [
+    {
+      name: "Popular",
+      icon: <FireOutlined />,
+    },
+    {
+      name: "Most unvoted",
+      icon: <ArrowUpOutlined />,
+    },
+    {
+      name: "Best discounts",
+      icon: <CreditCardOutlined />,
+    },
+    {
+      name: "Squad",
+      icon: <UngroupOutlined />,
+    },
+    {
+      name: "Search",
+      icon: <SearchOutlined />,
+    },
+  ];
+
+  const contributeData = [
+    {
+      name: "Submit article",
+      icon: <LinkOutlined />,
+    },
+    {
+      name: "Suggest new source",
+      icon: <CodeOutlined />,
+    },
+  ];
+
+  const manageData = [
+    {
+      name: "BookMarks",
+      icon: <BookOutlined />,
+    },
+    {
+      name: "Reading History",
+      icon: <EyeOutlined />,
+    },
+    {
+      name: "Pause new tab",
+      icon: <PauseCircleOutlined />,
+    },
+    {
+      name: "Customize",
+      icon: <SettingOutlined />,
+    },
+  ];
+
+  const footerData = [
+    {
+      name: "We are hiring",
+      icon: <SoundOutlined />,
+    },
+    {
+      name: "Docs",
+      icon: <FileTextOutlined />,
+    },
+    {
+      name: "Changelog",
+      icon: <MinusSquareOutlined />,
+    },
+    {
+      name: "Feedback",
+      icon: <AlertOutlined />,
+    },
+    {
+      name: "Invite People",
+      icon: <ShareAltOutlined />,
+    },
+  ];
+  //   const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <div
+      className="bg-black flex flex-1 flex-col"
+      style={{ justifyContent: "space-between" }}
+    >
+      <div className="mt-20" style={{ flex: 0.5 }}>
+        {feedData.map((list, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+            className="my-1"
+          >
+            {React.cloneElement(list.icon, {
+              style: { fontSize: "20px", color: "gray" },
+            })}
+            <div
+              className="text-base ml-4 text-slate-400 "
+              style={{ alignSelf: "center" }}
+            >
+              {list.name}
+            </div>
+          </div>
+        ))}
+        <div className="text-slate-700 text-sm font-bold">Discover</div>
+        {discoverData.map((list, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+            className="my-2"
+          >
+            {React.cloneElement(list.icon, {
+              style: { fontSize: "20px", color: "gray" },
+            })}
+            <div className="text-base ml-2 text-slate-400">{list.name}</div>
+          </div>
+        ))}
+        <div className="text-gray-700 text-sm font-bold">Contribute</div>
+        {contributeData.map((list, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+            className="my-2"
+          >
+            {React.cloneElement(list.icon, {
+              style: { fontSize: "20px", color: "gray" },
+            })}
+            <div className="text-base ml-2 text-slate-400">{list.name}</div>
+          </div>
+        ))}
+        <div className="text-gray-700 text-sm font-bold">Manage</div>
+        {manageData.map((list, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+            className="my-2"
+          >
+            {React.cloneElement(list.icon, {
+              style: { fontSize: "20px", color: "gray" },
+            })}
+            <div className="text-base ml-2 text-slate-400">{list.name}</div>
+          </div>
+        ))}
+      </div>
+      <div style={{ flex: 0.1, padding: "60px 0" }} />
+      <div style={{ flex: 0.2 }}>
+        {footerData.map((list, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+            className="my-2"
+          >
+            {React.cloneElement(list.icon, {
+              style: { fontSize: "20px", color: "gray" },
+            })}
+            <div className="text-base ml-2 text-slate-400">{list.name}</div>
+          </div>
+        ))}
+      </div>
+      <div className="border-2 text-slate-400 border-slate-400 p-2 w-44 flex flex-row justify-center rounded-xl mt-4">
+        <img src="../assets/images/daily.png" />
+        <div className="flex-col ml-3">
+          <div className="text-base">No rank</div>
+          <div className="text-xs">Re-earn: 0/1 days</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
